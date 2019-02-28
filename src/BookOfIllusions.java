6fc11c5b8d5fbc1b3ae58a13907bc494d34c5279
Schurter.Caitlyn <Schurter.Caitlyn@SEMS-259-33.SEMS259.LAB>
2019-02-27 16:28:20 -0800

    Finished Optical Illusions & Typing Tutor   2/27/19

--- src/BookOfIllusions.java
+++ src/BookOfIllusions.java
@@ -10,15 +10,20 @@ import java.net.URL;
 
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
- 
-/** We’re going to make a slideshow of cool optical illusions. When the user clicks on an illusion, a new one will be loaded. **/
+
+/**
+ * We’re going to make a slideshow of cool optical illusions. When the user
+ * clicks on an illusion, a new one will be loaded.
+ **/
 
 public class BookOfIllusions extends MouseAdapter {
 
 	/*
-	 * Here we are instantiating our BookOfIllusions class and calling it’s createBook() method. This is because we want to get out of the
-	 * static main method, so that we can add a click listener to each illusion.
+	 * Here we are instantiating our BookOfIllusions class and calling it’s
+	 * createBook() method. This is because we want to get out of the static main
+	 * method, so that we can add a click listener to each illusion.
 	 */
 
 	public static void main(String[] args) throws Exception {
@@ -27,30 +32,64 @@ public class BookOfIllusions extends MouseAdapter {
 	}
 
 	// 1. Make a JFrame variable and initialize it using "new JFrame()"
+	JFrame frame;
+	JLabel label;
+	String optical1;
+	String optical2;
+	int image = 1;
 
 	private void createBook() {
+		frame = new JFrame();
 		// 2. make the frame visible
+		frame.setVisible(true);
 		// 3. set the size of the frame
+		frame.setSize(500, 500);
 		// 4. find 2 images and save them to your project’s default package
+
 		// 5. make a variable to hold the location of your image. e.g. "illusion.jpg"
+		optical1 = "Optical_Illusion_1.jpg";
 		// 6. create a variable of type "JLabel" but don’t initialize it yet
+		label = new JLabel();
+		label = loadImageFromComputer(optical1);
 		// 7. use the "loadImage..." methods below to initialize your JLabel
+
 		// 8. add your JLabel to the frame
+		frame.add(label);
 		// 9. call the pack() method on the frame
+		frame.pack();
 		// 10. add a mouse listener to your frame (hint: use *this*)
+		frame.addMouseListener(this);
 	}
 
 	public void mousePressed(MouseEvent e) {
+
 		// 11. Print "clicked!" to the console when the mouse is pressed
+		System.out.println("Clicked!");
 		// 12. remove everything from the frame that was added earlier
+		frame.remove(label);
 		// 13. load a new image like before (this is more than one line of code)
+		if (image == 1) {
+			optical2 = "Optical_Illusion_2.jpg";
+			label = loadImageFromComputer(optical2);
+			image = 0;
+		}
+		
+		else {
+			optical1 = "Optical_Illusion_1.jpg";
+			label = loadImageFromComputer(optical1);
+			image = 1;
+		}
+		frame.add(label);
 		// 14. pack the frame
+		frame.pack();
+
 	}
 
 	// [OPTIONAL] 15. goad your users with some annoying or witty pop-ups
 
 	/*
-	 * To use this method, the image must be placed in your Eclipse project under "default package".
+	 * To use this method, the image must be placed in your Eclipse project under
+	 * "default package".
 	 */
 	public JLabel loadImageFromComputer(String fileName) {
 		URL imageURL = getClass().getResource(fileName);
@@ -59,5 +98,3 @@ public class BookOfIllusions extends MouseAdapter {
 	}
 
 }
-
-
--- src/DaTypingTutor.java
+++ src/DaTypingTutor.java
@@ -1,47 +1,65 @@
 import java.awt.Color;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.Date;
 import java.util.Random;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-public class DaTypingTutor implements KeyListener{
+public class DaTypingTutor implements KeyListener {
 	JFrame frame;
 	JPanel panel;
 	JLabel label;
 	char currentLetter = generateRandomLetter();
-	
-	
+	int lettersTyped;
+	float lettersCorrect;
+
 	public static void main(String[] args) {
 		new DaTypingTutor().makeFrame();
+		
 	}
-	
+
 	void makeFrame() {
+		lettersCorrect = 0;
+		lettersTyped = 0;
+		
 		frame = new JFrame();
 		frame.setVisible(true);
 		frame.setSize(500, 500);
-		
+
 		panel = new JPanel();
 		panel.setVisible(true);
 		frame.add(panel);
-		
+
 		label = new JLabel();
 		label.setVisible(true);
-		label.setText("W");  //Make the frame big enough
+		label.setText("W"); // Make the frame big enough
 		label.setFont(label.getFont().deriveFont(301.0f));
 		label.setHorizontalAlignment(JLabel.CENTER);
 		panel.add(label);
-		
+
 		frame.addKeyListener(this);
 		frame.pack();
 		label.setText(Character.toString(currentLetter));
 	}
-	
+
 	char generateRandomLetter() {
-	      Random r = new Random();
-	      return (char) (r.nextInt(26) + 'a');
+		Random r = new Random();
+		return (char) (r.nextInt(26) + 'a');
+	}
+
+	Date timeAtStart = new Date();
+
+	private void showTypingSpeed(int numberOfCorrectCharactersTyped) {
+		Date timeAtEnd = new Date();
+		long gameDuration = timeAtEnd.getTime() - timeAtStart.getTime();
+		long gameInSeconds = (gameDuration / 1000) % 60;
+		double charactersPerSecond = ((double) numberOfCorrectCharactersTyped / (double) gameInSeconds);
+		int charactersPerMinute = (int) (charactersPerSecond * 60);
+		JOptionPane.showMessageDialog(null, "Your typing speed is " + charactersPerMinute + " characters per minute.");
+		JOptionPane.showMessageDialog(null, "Your accuracy is " + lettersCorrect + "%");
 	}
 
 	@Override
@@ -49,16 +67,25 @@ public class DaTypingTutor implements KeyListener{
 		// TODO Auto-generated method stub
 		char typedChar = e.getKeyChar();
 		System.out.println("Letter Typed: " + typedChar);
-		
-		if(currentLetter == typedChar) {
-			System.out.println("Correct!");
-			panel.setBackground(Color.GREEN);
+
+		if (lettersTyped == 100) {
+			showTypingSpeed(lettersTyped);
+			frame.dispose();
 		}
 		
+		if (currentLetter == typedChar) {
+			System.out.println("Correct!");
+			panel.setBackground(Color.GREEN);
+			lettersTyped++;
+			lettersCorrect++;
+		}
+
 		else {
 			System.out.println("Incorrect!");
 			panel.setBackground(Color.RED);
+			lettersTyped++;
 		}
+
 	}
 
 	@Override
@@ -71,6 +98,6 @@ public class DaTypingTutor implements KeyListener{
 	@Override
 	public void keyTyped(KeyEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 }
